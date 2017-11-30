9e2167c1852443c00b4125a22651dfedf2dd9e7f
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-11-30 08:00:48 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,28 +5,11 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 	// write your code here
-
-        Scanner data = new Scanner(new File("average.txt"));
-        int index = -1;
-
-        String dataSet[] = new String[10];
-
-        while(data.hasNext()){
-            index++;
-            String name = data.next();
-            int avg = 0;
-            int numberOfGrades = 0;
-            while(data.hasNextBigInteger()){
-                avg += data.nextInt();
-                numberOfGrades++;
-            }
-            avg /= numberOfGrades;
-
-            System.out.println(name + ", Average = " + avg);
-        }
-
-
+	
     }
+	private static String cat(){
+		
+	}
 }
