6bfc6229aea180cddd467995d6ba4456d90a926d
Aaron Alfonso <31370853+aaronalfonso@users.noreply.github.com>
2017-11-30 08:22:22 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,16 @@ public class Main {
 
     public static void main(String[] args)  {
 	// write your code here
-	
+	String t1 = cat(68, 108);
+	System.out.println(t1);
+	String t2 = cat(35, 59);
+	System.out.println(t2);
     }
-	private static String cat(){
-		
+	private static String cat(int start, int end){
+		Char s = start;
+		for(start, end, start++){
+			System.out.print(s);
+			s++;
+		}
 	}
 }
